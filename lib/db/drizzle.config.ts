013d3cab485212c73fd78224eef62951f5d336d0
Apolo <paolacrisnvs@gmail.com>
2026-07-23 14:43:05 -0300

    kacka

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,6 +1,13 @@
-import "dotenv/config";
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 import path from "path";
+
+// Carrega o .env da raiz do projeto
+config({
+  path: path.resolve(process.cwd(), ".env"),
+});
+
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
